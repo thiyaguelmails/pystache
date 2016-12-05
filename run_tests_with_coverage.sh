11c26b668fed926e1da542854b1c133316f8bc59
Thiyagu Loganathan <thiyaguelmails@gmail.com>
2016-12-05 22:35:28 +0530

    Updated PYTHONPATH

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages:/usr/local/lib/python2.7/dist-packages
+
 . ~/.virtualenvs/python2.7/bin/activate
 
 rm -f pep8.log pyflakes.log
