bb0826c417127660aa1243c34cae3ebc617ce3fa
Thiyagu Loganathan <thiyaguelmails@gmail.com>
2016-12-05 22:07:45 +0530

    Update run_tests_with_junit.sh

--- run_tests_with_junit.sh
+++ run_tests_with_junit.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e 
 
+export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages:/usr/local/lib/python2.7/dist-packages
+
 . ~/.virtualenvs/python2.7/bin/activate
 
 ./test.py
