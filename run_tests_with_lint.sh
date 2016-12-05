8984a529ec7da455c2acaabb0325c9514e65e12e
Thiyagu Loganathan <thiyaguelmails@gmail.com>
2016-12-05 22:23:47 +0530

    Set PYTHONPATH

--- run_tests_with_lint.sh
+++ run_tests_with_lint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-
+export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages:/usr/local/lib/python2.7/dist-packages
 . ~/.virtualenvs/python2.7/bin/activate
 set -e
 set -x
