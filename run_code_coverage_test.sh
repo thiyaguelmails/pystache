828bceef55dccb11ebf03423d6fca9bed1d79a56
Thiyagu Loganathan <thiyaguelmails@gmail.com>
2016-12-06 15:40:37 +0530

    Enabling Code Coverage check to pipeline script

---
+++ run_code_coverage_test.sh
@@ -0,0 +1,8 @@
+export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages:/usr/local/lib/python2.7/dist-packages
+
+. ~/.virtualenvs/python2.7/bin/activate
+
+PYTHONPATH=. python -m coverage run test.py
+
+python -m coverage xml -o coverage.xml
+python -m coverage html -d coverage
