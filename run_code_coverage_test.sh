cafcf477019797fab8f2b3f99c2c0a1cd6cb13f0
Thiyagu Loganathan <thiyaguelmails@gmail.com>
2016-12-06 16:34:51 +0530

    Update run_code_coverage_test.sh

--- run_code_coverage_test.sh
+++ run_code_coverage_test.sh
@@ -2,7 +2,9 @@ export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages:/usr/local/lib/py
 
 . ~/.virtualenvs/python2.7/bin/activate
 
-PYTHONPATH=. python -m coverage run test.py
+# PYTHONPATH=. python -m coverage run test.py
+
+python -m coverage run test.py
 
 python -m coverage xml -o coverage.xml
 python -m coverage html -d coverage
