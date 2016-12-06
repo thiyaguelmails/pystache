2e61dd49534ead38d969b28eb764e08a930a1396
Thiyagu Loganathan <thiyaguelmails@gmail.com>
2016-12-06 16:40:39 +0530

    Enabling Cobertura support for Pipeline

--- run_code_coverage_test.sh
+++ run_code_coverage_test.sh
@@ -2,9 +2,11 @@ export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages:/usr/local/lib/py
 
 . ~/.virtualenvs/python2.7/bin/activate
 
+rm -f coverage.xml coverage clover.xml
 # PYTHONPATH=. python -m coverage run test.py
-
 python -m coverage run test.py
 
 python -m coverage xml -o coverage.xml
-python -m coverage html -d coverage
+# python -m coverage html -d coverage
+
+cobertura-clover-transform coverage.xml > clover.xml
