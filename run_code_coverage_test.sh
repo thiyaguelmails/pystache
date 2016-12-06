6393868b94a66cb662628f2a979d204c76f53b43
Thiyagu Loganathan <thiyaguelmails@gmail.com>
2016-12-06 17:02:14 +0530

    Update run_code_coverage_test.sh

--- run_code_coverage_test.sh
+++ run_code_coverage_test.sh
@@ -2,7 +2,7 @@ export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages:/usr/local/lib/py
 
 . ~/.virtualenvs/python2.7/bin/activate
 
-rm -f coverage.xml coverage clover.xml
+rm -rf coverage.xml coverage clover.xml
 # PYTHONPATH=. python -m coverage run test.py
 python -m coverage run test.py
 
