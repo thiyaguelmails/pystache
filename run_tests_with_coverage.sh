f53aca10854e2b608128a8a6a20442e341b89f4e
Thiyagu Loganathan <thiyaguelmails@gmail.com>
2016-12-06 10:13:00 +0530

    Fixed issue

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -4,14 +4,14 @@ export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages:/usr/local/lib/py
 
 . ~/.virtualenvs/python2.7/bin/activate
 
-rm -f pep8.log pyflakes.log
+rm -rf pep8.log pyflakes.log python_tests_xml
 
 PYTHONPATH=. python -m coverage run test.py
 
 python -m coverage xml -o coverage.xml
 python -m coverage html -d coverage
 
-./test.py
+# ./test.py
 
 pep8 --max-line-length=120 pystache > pep8.log || true
 pyflakes pystache > pyflakes.log || true
